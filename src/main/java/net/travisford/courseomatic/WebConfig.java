145d9e9b2998fdb1c756a53860191857972bfb7e
Travis <travford@gmail.com>
2020-11-27 17:48:03 -0500

    HW9 Commit.

--- src/main/java/net/travisford/courseomatic/WebConfig.java
+++ src/main/java/net/travisford/courseomatic/WebConfig.java
@@ -13,5 +13,6 @@ public class WebConfig implements WebMvcConfigurer
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
 
+
     }
 }
