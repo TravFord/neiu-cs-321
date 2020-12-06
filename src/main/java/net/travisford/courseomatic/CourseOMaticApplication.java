ee746c6e1e38d26e1fadfaebc094b0d4ad6f28e5
Travis <travford@gmail.com>
2020-12-06 15:15:24 -0500

    Added CourseOMaticApplication to Git. wth.

---
+++ src/main/java/net/travisford/courseomatic/CourseOMaticApplication.java
@@ -0,0 +1,13 @@
+package net.travisford.courseomatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseOMaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseOMaticApplication.class, args);
+    }
+
+}
