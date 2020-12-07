ddcf54a9c9c10511348ea198bb6a8443eb824586
Rachel Trana <raxytrana@gmail.com>
2020-12-06 18:44:41 -0600

    Commenting out proper part of data-mysql file

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -16,79 +16,79 @@ INSERT INTO Courses (title, courseNumber, dept) VALUES('Server Side Web Dev','32
 INSERT INTO Courses (title, courseNumber, dept) VALUES('Intro To Design Of Algorithms','324','CS');
 INSERT INTO Courses (title, courseNumber, dept) VALUES('English','101','ENGL');
 
-
------ Programming 2 ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '201' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '200' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
------ Client Side ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '300' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '201' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '200' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
------ Computer Org ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '301' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '201' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '200' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
------ Data Structures ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '304' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '201' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
------ Operating systems ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '308' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '301' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
------ Database ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '315' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
------ Software Engineering ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '319' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '304' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '101' AND dept = 'ENGL';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
------ Server Side ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '321' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '300' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
-
------ Intro To Design Of Algorithms ------
-SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '324' AND dept = 'CS';
-
-SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '304' AND dept = 'CS';
-INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Programming 2 ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '201' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '200' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Client Side ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '300' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '201' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '200' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Computer Org ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '301' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '201' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '200' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Data Structures ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '304' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '201' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Operating systems ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '308' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '301' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Database ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '315' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Software Engineering ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '319' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '304' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '101' AND dept = 'ENGL';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Server Side ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '321' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '207' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '300' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
+--
+-- ----- Intro To Design Of Algorithms ------
+-- SET @courseID = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '324' AND dept = 'CS';
+--
+-- SET @prereqId = SELECT MAX(courseID) FROM Courses WHERE courseNumber = '304' AND dept = 'CS';
+-- INSERT INTO Course_Prereq(courseId, prereqId) VALUES(@courseID, @prereqId);
 
